7e67079636ced9613be1e66962b06ab3e7881a2a
Wiktor <wiktor1474@wp.pl>
2022-09-26 08:02:23 +0200

    backend_validation_of_loan_amount_&_error_handler

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+use App\Model\FeeCalculator;
+use App\Model\LoanProposal;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ResultController extends AbstractController
 {
+    private FeeCalculator $feeCalculator;
+    private ValidatorInterface $validator;
+
+    public function __construct(FeeCalculator $feeCalculator, ValidatorInterface $validator)
+    {
+        $this->feeCalculator = $feeCalculator;
+        $this->validator = $validator;
+    }
+
     /**
      * @Route("/result", name="app_result")
      */
     public function index(): Response
     {
+        $loanAmount = 3000;
+
+        $application = new LoanProposal($loanAmount);
+
+        $validation = $this->validator->validate($application);
+        if($validation->count())
+        {
+            return $this->render('error/index.html.twig');
+        }
+
+        $fee = $this->feeCalculator->calculate($application);
+
         return $this->render('result/index.html.twig', [
-            'controller_name' => 'ResultController',
+            'fee' => $fee,
         ]);
     }
 }
--- src/Model/LoanProposal.php
+++ src/Model/LoanProposal.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * A cut down version of a loan application containing
@@ -11,6 +12,17 @@ namespace App\Model;
  */
 class LoanProposal
 {
+    const MIN = 1000;
+    const MAX = 20000;
+
+    /**
+     * @var float
+     * @Assert\Range(
+     *      min = self::MIN,
+     *      max = self::MAX
+     * )
+     */
+
     private float $amount;
 
     public function __construct(float $amount)
